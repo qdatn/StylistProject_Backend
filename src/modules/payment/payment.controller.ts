0885de101ce1bcd162b0296d6e88d88734e2f921
datnq <datnq2003@gmail.com>
2024-11-28 23:29:40 +0700

    updat redirect url  momo payment controller

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -18,7 +18,7 @@ class PaymentController {
       const accessKey = "F8BBA842ECF85";
       const secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
       const partnerCode = "MOMO";
-      const redirectUrl = "http://localhost:3000/payment";
+      const redirectUrl = "http://localhost:3000/payment/success";
       const ipnUrl =
         "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
       const orderId = order_id + new Date().getTime();
