734c485b81c3445eb27c0bbf295bdf458310594b
datnq <datnq2003@gmail.com>
2024-12-22 22:02:04 +0700

    Get voucher for selected product in cart

--- src/modules/discount/discount.service.ts
+++ src/modules/discount/discount.service.ts
@@ -1,3 +1,5 @@
+import { Product } from "@modules/product";
+import Discount from "./discount.model";
 import DiscountRepository from "./discount.repository";
 import DiscountDto from "./dtos/discount.dto";
 
@@ -20,6 +22,40 @@ class DiscountService {
 
   async deleteDiscount(id: string) {
     return await DiscountRepository.delete(id);
+  }
+
+  async getAvailableDiscounts(productIds: string[], totalPrice: number) {
+    const discounts = await Discount.find({
+      $or: [
+        { type: "all" },
+        { type: "product", apply_items: { $in: productIds } },
+        {
+          type: "category",
+          apply_items: {
+            $in: await this.getCategoryIdsFromProductIds(productIds),
+          },
+        },
+      ],
+      start_date: { $lte: new Date() },
+      end_date: { $gte: new Date() },
+      status: true,
+    });
+
+    return discounts.filter((discount) => {
+      // Check conditions for minimum value and max discount
+      if (discount.minimum_value && totalPrice < discount.minimum_value) {
+        return false;
+      }
+      return true;
+    });
+  }
+
+  private async getCategoryIdsFromProductIds(productIds: string[]) {
+    const products = await Product.find({ _id: { $in: productIds } }).select(
+      "categories"
+    );
+    const categoryIds = products.flatMap((product) => product.categories);
+    return [...new Set(categoryIds.map((id) => id.toString()))]; // Remove duplicates
   }
 }
 
