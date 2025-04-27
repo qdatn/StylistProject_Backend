c44ac768dee7dd2bb519c16738ec8752b65a9825
datnq <datnq2003@gmail.com>
2025-04-27 17:09:28 +0700

    feat: add google login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,6 +7,9 @@ import IAuth from "./auth.interface";
 // import AuthDto from "./dtos/auth.dto";
 import jwt from "jsonwebtoken";
 import generateJwt from "@core/utils/generateJwt";
+import axios from "axios";
+import { UserInfo } from "@modules/userInfo";
+import User from "./user.model";
 
 class AuthController {
   async register(
@@ -25,6 +28,62 @@ class AuthController {
       //   res.status(400).json({ message: result.message });
       // }
     } catch (error: any) {
+      next(error);
+    }
+  }
+
+  async googleLogin(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { token } = req.body;
+
+      // Xác thực token từ Google
+      const response = await axios.get(
+        `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`
+      );
+      const googleUser = response.data;
+
+      // Kiểm tra người dùng đã tồn tại trong DB chưa
+      let user = await User.findOne({ email: googleUser.email });
+
+      if (!user) {
+        // Nếu người dùng chưa tồn tại, tạo mới thông qua register
+        // Tạo đối tượng user để gửi vào đăng ký
+        const registerDto = {
+          name: googleUser.name,
+          email: googleUser.email,
+          password: "1234", // Mật khẩu tạm thời
+          role: "customer", // Hoặc xác định vai trò tùy theo yêu cầu
+        };
+
+        // Gọi hàm register từ AuthService để đăng ký người dùng
+        await AuthService.register(registerDto);
+
+        // Sau khi đăng ký, tìm lại người dùng trong DB
+        user = await User.findOne({ email: googleUser.email });
+      }
+
+      // Tạo JWT token cho người dùng
+      if (user) {
+        const tokenJWT = generateJwt(user);
+        // Lưu token vào cookie
+        res.cookie("token", tokenJWT, {
+          httpOnly: true, // Không thể truy cập cookie từ JavaScript
+          secure: process.env.NODE_ENV === "production", // Chỉ gửi cookie qua HTTPS trong môi trường production
+          maxAge: 60 * 60 * 24 * 1000, // Cookie tồn tại 1 ngày
+        });
+
+        // Trả về user và token
+        res.status(200).json({
+          message: "Login success",
+          token: tokenJWT,
+          user,
+        });
+      }
+    } catch (error) {
       next(error);
     }
   }
