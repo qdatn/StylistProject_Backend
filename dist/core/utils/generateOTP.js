26ca5439b2d4a986e3e072361a235479d96bf1d2
datnq <datnq2003@gmail.com>
2025-04-28 22:18:01 +0700

    Remove dist folder from tracking

--- dist/core/utils/generateOTP.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = generateOTP;
-function generateOTP() {
-    return Math.floor(100000 + Math.random() * 900000); // 6 chữ số ngẫu nhiên
-}
-//# sourceMappingURL=generateOTP.js.map
