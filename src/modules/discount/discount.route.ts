d3fb86e00ec57b4158691f25a39042585839331b
datnq <datnq2003@gmail.com>
2025-05-11 15:11:16 +0700

    fix: get available api

--- src/modules/discount/discount.route.ts
+++ src/modules/discount/discount.route.ts
@@ -13,8 +13,14 @@ class DiscountRoute implements RouteInterface {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/`, DiscountController.getAllDiscounts);
-    this.router.get(`${this.path}/available-discounts`, DiscountController.getAvailableDiscounts);
-    this.router.post(`${this.path}/apply-discount`, DiscountController.applyDiscount);
+    this.router.post(
+      `${this.path}/available-discounts`,
+      DiscountController.getAvailableDiscounts
+    );
+    this.router.post(
+      `${this.path}/apply-discount`,
+      DiscountController.applyDiscount
+    );
     this.router.get(`${this.path}/:id`, DiscountController.getDiscountById);
     this.router.post(`${this.path}/`, DiscountController.createDiscount);
     this.router.put(`${this.path}/:id`, DiscountController.updateDiscount);
