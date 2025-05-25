1b86fe6153f692fa8e03608550da6e3ee9a3f566
datnq <datnq2003@gmail.com>
2025-05-25 18:52:59 +0700

    fix get all noti by userinfi._id

--- src/modules/notification/notification.repository.ts
+++ src/modules/notification/notification.repository.ts
@@ -4,37 +4,37 @@ import NotificationDto from "./dtos/notification.dto";
 
 class NotificationRepository {
   async findAll() {
-    // return await Notification.find();
-    return await Notification.aggregate([
-      {
-        $lookup: {
-          from: "userinfos", // tên collection (viết thường và thêm "s")
-          localField: "user", // Notification.user
-          foreignField: "user", // UserInfo.user
-          as: "user",
-        },
-      },
-      {
-        $unwind: {
-          path: "$user",
-          preserveNullAndEmptyArrays: true, // nếu không tìm thấy UserInfo
-        },
-      },
-      {
-        $lookup: {
-          from: "orders", // nếu bạn cũng muốn populate thêm "order"
-          localField: "order",
-          foreignField: "_id",
-          as: "order",
-        },
-      },
-      {
-        $unwind: {
-          path: "$order",
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-    ]);
+    return await Notification.find().populate("user");
+    // return await Notification.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: "userinfos", // tên collection (viết thường và thêm "s")
+    //       localField: "user", // Notification.user
+    //       foreignField: "user", // UserInfo.user
+    //       as: "userInfo",
+    //     },
+    //   },
+    //   // {
+    //   //   $unwind: {
+    //   //     path: "$user",
+    //   //     preserveNullAndEmptyArrays: true, // nếu không tìm thấy UserInfo
+    //   //   },
+    //   // },
+    //   {
+    //     $lookup: {
+    //       from: "orders", // nếu bạn cũng muốn populate thêm "order"
+    //       localField: "order",
+    //       foreignField: "_id",
+    //       as: "order",
+    //     },
+    //   },
+    //   // {
+    //   //   $unwind: {
+    //   //     path: "$order",
+    //   //     preserveNullAndEmptyArrays: true,
+    //   //   },
+    //   // },
+    // ]);
   }
 
   async findById(id: string) {
