947fcfc732189fb4e1b015ceb3aa243ae1901623
datnq <datnq2003@gmail.com>
2025-06-14 12:25:36 +0700

    deploy 3

--- dist/core/utils/generateGroupIdForChat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function generateGroupId(id1, id2) {
-    // Sắp xếp senderId và receiverId theo thứ tự alphabet để đảm bảo groupId nhất quán
-    const sortedIds = [id1, id2].sort();
-    return sortedIds.join("_");
-}
-exports.default = generateGroupId;
-//# sourceMappingURL=generateGroupIdForChat.js.map
--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -19,7 +19,7 @@ class PaymentController {
       const secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
       const partnerCode = "MOMO";
       const redirectUrl =
-        "https://stylist-project-frontend.vercel.app/payment/success";
+        "http://localhost:3000/payment/success";
       const ipnUrl =
         "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
       const orderId = order_id + new Date().getTime();
