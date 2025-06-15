08c2c9dda4ed31e3f640532045cf45aa411ac3b8
datnq <datnq2003@gmail.com>
2025-06-15 17:12:12 +0700

    delete .env on remote repo

--- dist/modules/payment/payment.controller.js
+++ dist/modules/payment/payment.controller.js
@@ -19,11 +19,14 @@ class PaymentController {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 const { order_id, amount, orderInfo, requestType, extraData, autoCapture, } = req.body;
+                const frontendUrl = process.env.BASE_URL;
                 // Các tham số cố định
                 const accessKey = "F8BBA842ECF85";
                 const secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
                 const partnerCode = "MOMO";
-                const redirectUrl = "http://localhost:3000/payment/success";
+                const redirectUrl = 
+                // `http://localhost:3000/payment/success`;
+                `${frontendUrl}/payment/success`;
                 const ipnUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
                 const orderId = order_id + new Date().getTime();
                 const requestId = orderId;
